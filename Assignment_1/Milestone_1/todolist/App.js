5c1528cf72d58e238e2c26e700f2ee8e4781aa47
Cameron Francis <cameron.francis@griffith.edu.au>
2024-04-14 11:39:18 +1000

    create container for todolist

--- Assignment_1/Milestone_1/todolist/App.js
+++ Assignment_1/Milestone_1/todolist/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>My Todo List</Text>
+      <Text style={styles.todoItem}>Todo Item 1</Text>
+      <Text style={styles.todoItem}>Todo Item 2</Text>
+      <Button title="Add New Todo" onPress={() => {}} />
     </View>
   );
 }
